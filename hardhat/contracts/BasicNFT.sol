4499e0fc3c5ff139c21b3d97bd7ae7b81bc27938
Marek Choiński <marekchoinski17@gmail.com>
2023-07-05 16:22:18 +0200

    OB-1 Create basic nft

---
+++ hardhat/contracts/BasicNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeibmfc6ybg2ktxyullwqxtkssktmbs7ac57sxhf26ydgzbdnzu6xve";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function tokenURI(uint256 tokenId) public pure override returns (string memory) {
+        // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
---
+++ hardhat/deploy/01-mint-basic-nft.js
@@ -0,0 +1,13 @@
+const { network, ethers } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts }) => {
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // Basic NFT
+    const basicNft = await ethers.getContract("BasicNft", deployer)
+    const basicMintTx = await basicNft.mintNft()
+    await basicMintTx.wait(1)
+    console.log(`Basic NFT index 0 tokenURI: ${await basicNft.tokenURI(0)}`)
+}
+module.exports.tags = ["all", "mintBasic"]
